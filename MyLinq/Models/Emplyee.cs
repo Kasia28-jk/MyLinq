6e65e9c5626f636b1e3c36c89ba65c0dca82e49f
Katarzyna Norys <katanor457@student.polsl.pl>
2020-12-17 23:16:40 +0100

    Modifcation Employee class by feature

--- MyLinq/Models/Emplyee.cs
+++ MyLinq/Models/Emplyee.cs
@@ -8,5 +8,21 @@ namespace MyLinq.Models
 {
     class Emplyee
     {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public List<Note> Notes { get; set; }
+        public List<Job> Jobs { get; set; }
+        public string FullName
+        {
+            //get
+            //{
+            //    return String.Format("{0} {1}",
+            //      FirstName, LastName);
+            //}
+            // get => String.Format("{0} {1}",FirstName, LastName);
+            get => String.Format($"{FirstName} {LastName}");
+        }
     }
 }
